311438556232b7dd316eb697eab4b92a0edfbe84
va0r <i@vialor.ru>
2023-12-21 13:59:05 +0300

    code review

--- courses/tests/test_CoursesTestCase.py
+++ courses/tests/test_CoursesTestCase.py
@@ -40,3 +40,42 @@ class CoursesTestCase(APITestCase):
         # Попытка получить несуществующий курс
         response = self.client.get(reverse('courses:courses-detail', kwargs={'pk': 999}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_course(self):
+        new_course_data = {'name': 'New Course', 'description': 'New Course Description'}
+
+        response = self.client.post(reverse('courses:courses-list'), data=new_course_data, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Course.objects.count(), 2)  # Должен быть только один курс
+        new_course = Course.objects.get(name=new_course_data['name'])
+        self.assertEqual(response.data['name'], new_course.name)
+        self.assertEqual(response.data['description'], new_course.description)
+
+    def test_update_course(self):
+        updated_course_data = {'name': 'Updated Course', 'description': 'Updated Course Description'}
+
+        response = self.client.put(self.url, data=updated_course_data, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.name, updated_course_data['name'])
+        self.assertEqual(self.course.description, updated_course_data['description'])
+
+    def test_delete_course(self):
+        response = self.client.delete(self.url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Course.objects.filter(pk=self.course.pk).exists())
+
+    def test_perform_update(self):
+        updated_course_data = {'name': 'Updated Course', 'description': 'Updated Course Description'}
+
+        response = self.client.patch(self.url, data=updated_course_data, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.course.refresh_from_db()
+        self.assertEqual(self.course.name, updated_course_data['name'])
+        self.assertEqual(self.course.description, updated_course_data['description'])
+
+
